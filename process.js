2ac25d395a11c4bee0f3e521fdfa7d26b45d8ab6
Lalisfeed <59479762+Lalisfeed@users.noreply.github.com>
2021-02-10 08:42:27 +0530

    console changes

--- process.js
+++ process.js
@@ -3,4 +3,3 @@ function proArch(X) { return process.arch }
 function proPlatform(D) { return process.platform }
 function proShell(C) { return process.env.SHELL }
 module.exports = { Arch : proArch, Platform : proPlatform, Shell: proShell}
-console.log(proArch() + proPlatform() + proShell())
